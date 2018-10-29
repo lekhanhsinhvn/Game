12697ea00cb29c34155d75374508a2c503554341
haahha <thangnmd00688@fpt.edu.vn>
2018-10-29 19:50:27 +0700

    Login, Register Test

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-ECHO is on.
+// require('express-async-errors');
+// const winston = require('winston');
+// require('winston-mongodb');x
+const error = require('./middleware/error')
+const config = require('config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+const auth = require('./routes/auth')
+const express = require('express');
+const app = express();
+
+// winston.add(winston.transports.File, {filename: 'logfile.log'})
+// winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly'})
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined')
+  process.exit(1);
+}
+
+mongoose.connect('mongodb://localhost/GameCard', {useNewUrlParser: true})
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
+app.use('/api/users', users);
+app.use('/api/auth', auth)
+
+// app.use(error);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
